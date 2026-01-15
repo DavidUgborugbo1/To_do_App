b60e49adcaa966e0fdd612251abfbbf040ab6e89
Aethergeist <148887615+DavidUgborugbo1@users.noreply.github.com>
2026-01-15 22:36:28 +0100

    Updated changes to task_page

--- tasks_page.dart
+++ tasks_page.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:hive_flutter/hive_flutter.dart';
-
+import 'package:to_do_app/model/task.dart';
 class TasksPage extends StatefulWidget {
   const TasksPage({Key? key}) : super(key: key);
 
@@ -30,6 +30,7 @@ class _TasksPageState extends State<TasksPage> {
           ),
         ),
       ),
+
       body: _tasksWidget(),
         floatingActionButton: FloatingActionButton(
     onPressed: displayTaskPop,
@@ -38,6 +39,8 @@ class _TasksPageState extends State<TasksPage> {
 
     );
   }
+
+  
 
 
   Widget _tasksWidget(){
@@ -60,10 +63,20 @@ class _TasksPageState extends State<TasksPage> {
         title: const Text("Add a Task"),
         content: TextField(
           onSubmitted: (value){
-            setState(() {
-              print(value);
-              Navigator.pop(context);
-            });
+
+            if(content != null){
+              var task = Task(
+                  todo: content!,
+                  timeStamp: DateTime.now(),
+                  done: false);
+              _box!.add(task.toMap());
+
+              setState(() {
+                print(value);
+                Navigator.pop(context);
+              });
+            }
+
           },
 
           onChanged: (value){
